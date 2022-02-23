6c4d75a79233ba7f60f19f301203aee92a68e250
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-02-23 22:02:30 +0100

    Refactor of input Moved both reading and processing of input into separate functions thus simplifying main.

--- main.c
+++ main.c
@@ -38,23 +38,31 @@ void enableRawMode() {
         terminate("tcsetattr");
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if(nread == -1 && errno != EAGAIN)
+            terminate("read");
+    }
+    return c;
+}
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
 int main() {
     enableRawMode();
 
     while (1) {
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            terminate("read");
-
-        if(iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-
-        if(c == CTRL_KEY('q'))
-            exit(0);
-
+        editorProcessKeypress();
     }
 
     return 0;
