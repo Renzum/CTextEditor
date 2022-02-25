73c6d59a253790847e768c9f47806355c8a0f497
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-02-25 19:47:13 +0100

    Added Buffer and Editor Version

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <string.h>
+
+#define EDITOR_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -101,24 +104,65 @@ int getWindowSize(int* rows, int* cols) {
     }
 }
 
-void editorDrawRows() {
+struct abuf {
+    char* b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf* ab, const char* str, int len) {
+    char* new = realloc(ab->b, ab->len + len);
+
+    if(!new) return;
+
+    memcpy(&new[ab->len], str, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf* ab) {
+    free(ab->b);
+}
+void editorDrawRows(struct abuf* ab) {
     int y;
     for(y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        if(y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                    "Text Editor -- version %s", EDITOR_VERSION);
+            if(welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if(padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
 
+        abAppend(ab, "\x1b[K", 3);
         if(y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
 
-    editorDrawRows();
+    abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[H", 3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 void editorProcessKeypress() {
